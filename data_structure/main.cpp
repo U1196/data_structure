616f617b64057924048bcfadca71d988720393c9
Fushimi <1978772030@qq.com>
2025-03-15 10:21:37 +0800

    Initial Commit

---
+++ data_structure/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  data_structure
+//
+//  Created by Fushimi on 2025/3/15.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
